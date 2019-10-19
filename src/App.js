285c3fe379491bf63f5fa783a5fdfb5685284c47
Frances Cancio <frances.cancio@gmail.com>
2019-10-18 20:23:48 -0500

    fetches recipes from edamam

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect , useState } from 'react';
 import './App.css';
 
 const App = () => {
   const APP_ID = process.env.REACT_APP_APP_ID;
   const API_KEY = process.env.REACT_APP_API_KEY;
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`
+useEffect( () =>{
+  getRecipes();
+},[])
+
+const getRecipes = async () => {
+  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`)
+  const data = await response.json();
+  console.log(data);
+}
+
   return(
     <div className="App">
       <form className="search-form" type="text">
         <input className="search-bar" type="text" />
-        <button className="search-button" type="submit">
+        <button 
+          className="search-button" 
+          type="submit">
           Search
         </button>
       </form>
